0ad2d4dc8fa82117a0380222ddd3d8c365d7be77
Jonny <giancarlopretell1@gmail.com>
2024-03-10 16:59:43 -0400

    CodingRooms

---
+++ dog/DogStats.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+public class DogStats {
+    public static void main(String[] args) {
+        //Create the scanner
+        Scanner scanner = new Scanner(System.in);
+
+        //Ask for dog's name
+        System.out.println("What is the name of your dog?");
+        String dogName = scanner.nextLine();
+
+        //Ask for dog's breed
+        System.out.println("What type of dog is " + dogName + "?");
+        String dogBreed = scanner.nextLine();
+
+        //Create onbject for my dog
+        Dog myDog = new Dog(dogName, dogBreed);
+
+        //Display the output using the Dog class
+        System.out.println("The dog's name is: " + myDog.getName());
+        System.out.println("The dog's breed is: " + myDog.getBreed());
+    }
+}
+
